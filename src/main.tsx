7c14c37c4e003f3cecf577bfd5ef9973a3ceb9b3
Elizabeth Kritskaya <post.kri@gmail.com>
2025-12-24 23:17:17 +0300

    add storybook + style

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
