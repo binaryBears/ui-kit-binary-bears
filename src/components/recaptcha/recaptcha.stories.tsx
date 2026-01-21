f6422703f1d4e554a390584fd1427606c09b98e2
OlhaFedorets <fedoretsolha@gmail.com>
2026-01-21 18:43:22 +0300

    IN-64-Recaptcha-library feat: recaptcha with react-google-recaptcha library

---
+++ src/components/recaptcha/recaptcha.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { Recaptcha } from './recaptcha'
+
+const meta = {
+    component: Recaptcha,
+    tags: ['autodocs'],
+    title: 'Components/Recaptcha',
+} satisfies Meta<typeof Recaptcha>
+
+export default meta
+type Story = StoryObj<typeof Recaptcha>
+
+const sitekey = '6LdDzWorAAAAAPcxJrGwISEs6xoa8Pw4Zy1jCbmP'
+
+export const Default: Story = {
+    args: {
+        onChange: token => console.log('Verified:', token),
+        sitekey,
+    },
+}
+export const RecaptchaWithError: Story = {
+    args: {
+        error: 'Please verify that you are not a robot',
+        onChange: token => console.log('Verified:', token),
+        sitekey,
+    },
+}
