7e1231ae85fc1cd5c82bfd7a511c35642e5bf194
Mger-arzi <mger54598@gmail.com>
2026-01-24 15:24:01 +0300

    n

--- src/components/checkBox/checkbox.tsx
+++ src/components/checkBox/checkbox.tsx
@@ -2,7 +2,6 @@ import { forwardRef, type ComponentPropsWithoutRef, type ReactNode } from 'react
 import { useGenerateId } from '../../hooks/useGenerateId'
 import clsx from 'clsx'
 
-
 import s from './checkbox.module.scss'
 
 export type CheckboxProps = {
